18c837d7d0ea7709b0372a41e80b69a2f4e8aca2
makiomar <mo7amed.maki@gmail.com>
2024-10-02 16:17:38 +0300

    Ability to edit the game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -9,6 +9,25 @@ class Game extends Model
 {
     use HasFactory;
 
+    // Define the fillable fields (excluding _token)
+    protected $fillable = [
+        'title',
+        'code',
+        'full_price',
+        'ps4_primary_price',
+        'ps4_primary_status',
+        'ps4_secondary_price',
+        'ps4_secondary_status',
+        'ps4_offline_price',
+        'ps4_offline_status',
+        'ps5_primary_price',
+        'ps5_primary_status',
+        'ps5_offline_price',
+        'ps5_offline_status',
+        'ps4_image_url',
+        'ps5_image_url'
+    ];
+
     /**
      * The accounts that belong to the game.
      */
