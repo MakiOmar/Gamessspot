4a2ae68f069ecfcf23d132c5e3028e541d152e3d
makiomar <mo7amed.maki@gmail.com>
2024-10-13 12:57:06 +0300

    fix: Profiles search result

--- resources/views/manager/partials/store_profile_rows.blade.php
+++ resources/views/manager/partials/store_profile_rows.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td>{{ $storeProfile->id }}</td>
     <td>{{ $storeProfile->name }}</td>
-    <td>{{ $storeProfile->phone }}</td>
+    <td>{{ $storeProfile->phone_number }}</td>
     <td>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editStoreProfileModal" data-id="{{ $storeProfile->id }}">Edit</button>
     </td>
