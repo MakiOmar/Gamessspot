a4db1fdedbddc85e1b8cc1d0522f04298868720f
makiomar <mo7amed.maki@gmail.com>
2024-10-14 10:26:43 +0300

    feat: Add store profile id to the order

---
+++ database/migrations/2024_10_14_070839_add_store_profile_id_to_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('store_profile_id')->nullable()->after('seller_id');
+            $table->foreign('store_profile_id')->references('id')->on('stores_profile')->onDelete('set null');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // Drop the foreign key and the column when rolling back
+            $table->dropForeign(['store_profile_id']);
+            $table->dropColumn('store_profile_id');
+        });
+    }
+};
