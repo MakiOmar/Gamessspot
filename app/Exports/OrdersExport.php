d0daceaf03977e3f59a1e9b00ae7ce3e6137c2f5
makiomar <mo7amed.maki@gmail.com>
2024-10-22 19:39:49 +0300

    Export orders by profile id

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -16,21 +16,26 @@ class OrdersExport implements FromCollection, WithHeadings
     public function collection()
     {
         return Order::join('accounts', 'orders.account_id', '=', 'accounts.id')
-                    ->join('users', 'orders.seller_id', '=', 'users.id')
-                    ->join('games', 'accounts.game_id', '=', 'games.id')  // Join with the games table
-                    ->select([
-                        'orders.id',
-                        'users.name as seller_name',  // Seller name
-                        'accounts.mail as account_mail',  // Account email
-                        'accounts.password',  // Account password
-                        'games.title as game_name',  // Game name
-                        'orders.buyer_phone',
-                        'orders.buyer_name',
-                        'orders.price',
-                        'orders.sold_item',
-                        'orders.notes',
-                        'orders.created_at',
-                    ])->get();
+            ->join('users', 'orders.seller_id', '=', 'users.id')
+            ->join('games', 'accounts.game_id', '=', 'games.id')  // Join with the games table
+            ->select([
+                'orders.id',
+                'users.name as seller_name',  // Seller name
+                'accounts.mail as account_mail',  // Account email
+                'accounts.password',  // Account password
+                'games.title as game_name',  // Game name
+                'orders.buyer_phone',
+                'orders.buyer_name',
+                'orders.price',
+                'orders.sold_item',
+                'orders.notes',
+                'orders.created_at',
+            ])
+            // Check if `$_GET['id']` is set and not empty, and apply the filter
+            ->when(!empty($_GET['id']), function ($query) {
+                $query->where('orders.store_profile_id', $_GET['id']);
+            })
+            ->get();
     }
 
     /**
