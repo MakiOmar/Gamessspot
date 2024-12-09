4d52e23fbcbfe7ba1ed7d478b206d4bb7c08d416
makiomar <mo7amed.maki@gmail.com>
2024-12-09 17:59:17 +0200

    Change images

--- resources/views/manager/dashboard-sales.blade.php
+++ resources/views/manager/dashboard-sales.blade.php
@@ -3,7 +3,9 @@
     <!-- Card for PS4 Games -->
     <div class="col-md-4">
         <div class="card">
-            <img src="{{ asset('assets/img/ps4.png') }}" class="card-img-top" alt="PS4 Games">
+            <a href="{{ route('manager.games.ps4') }}">
+                <img src="{{ asset('assets/img/ps4.jpg') }}" style="height:155px" class="card-img-top" alt="PS4 Games">
+            </a>
             <div class="card-body text-center">
                 <h5 class="card-title">PS4 Games</h5>
                 <p class="card-text">Explore and manage your PS4 games.</p>
@@ -15,7 +17,9 @@
     <!-- Card for PS5 Games -->
     <div class="col-md-4">
         <div class="card">
-            <img src="{{ asset('assets/img/ps5.png') }}" class="card-img-top" alt="PS5 Games">
+            <a href="{{ route('manager.games.ps5') }}">
+                <img src="{{ asset('assets/img/ps5.jpg') }}" style="height:155px" class="card-img-top" alt="PS5 Games">
+            </a>
             <div class="card-body text-center">
                 <h5 class="card-title">PS5 Games</h5>
                 <p class="card-text">Explore and manage your PS5 games.</p>
@@ -27,7 +31,9 @@
     <!-- Card for Selling Gift Cards -->
     <div class="col-md-4">
         <div class="card">
-            <img src="{{ asset('assets/img/giftcard.webp') }}" class="card-img-top" alt="Sell Gift Cards">
+            <a href="{{ route('manager.sell-cards') }}">
+                <img src="{{ asset('assets/img/giftcard.jpg') }}" style="height:155px" class="card-img-top" alt="Sell Gift Cards">
+            </a>
             <div class="card-body text-center">
                 <h5 class="card-title">Sell Gift Cards</h5>
                 <p class="card-text">Manage and sell gift cards to your customers.</p>
