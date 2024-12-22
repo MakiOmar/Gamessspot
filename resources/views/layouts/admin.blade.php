038a758df794e3e503d545d6477fd417ab12f363
makiomar <mo7amed.maki@gmail.com>
2024-12-23 00:08:07 +0200

    Add bootstrap icons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -100,6 +100,7 @@
 <script src="{{ asset('assets/js/sweetalert2.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('assets/css/flatpickr.css') }}">
 <link rel="stylesheet" href="{{ asset('build/assets/app-DqME6eCz.css') }}">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous"><!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
 <style type="text/css">
 
     {{-- You can add AdminLTE customizations here --}}
