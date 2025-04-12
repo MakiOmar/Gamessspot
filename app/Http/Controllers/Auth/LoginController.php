4ffc20693bb111124d8b8387c812d54245a0cf3e
makiomar <mo7amed.maki@gmail.com>
2025-04-12 12:53:04 +0200

    Change auth redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/manager';
 
     /**
      * Create a new controller instance.
