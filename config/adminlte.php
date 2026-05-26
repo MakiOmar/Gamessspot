45645a6ee76e9603b0cb238838ffd9b20896a714
TaskFlow Developer <developer@taskflow.app>
2026-05-26 22:55:28 +0300

    Disable role/permissions management

--- config/adminlte.php
+++ config/adminlte.php
@@ -514,14 +514,6 @@ return [
             ],
         ],
 
-        // Roles & Permissions
-        [
-            'text' => 'Roles & Permissions',
-            'route' => 'manager.roles-permissions.index',
-            'icon' => 'bi bi-shield-lock',
-            'can' => 'manage-options',
-        ],
-
         // Settings
         [
             'text' => 'Settings',
